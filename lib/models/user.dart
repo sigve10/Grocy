cda76329018afe0c58d9394ff022a236a78488d4
stenOskar <stenhalse@gmail.com>
2024-10-25 14:27:46 +0200

    Implemented user model

---
+++ lib/models/user.dart
@@ -0,0 +1,12 @@
+/// Implement user model class
+class User {
+  String name;
+  String email;
+  String password;
+
+  /// User constructor
+  User({required this.name,
+    required this.email,
+    required this.password
+  });
+}
