b4834dbc8c9406f71a7ee82774fbaa83c9bdc8aa
stenOskar <stenhalse@gmail.com>
2024-10-25 14:20:38 +0200

    Implemented boiler plate code for rating.dart

---
+++ lib/models/rating.dart
@@ -0,0 +1,17 @@
+/// Rating model class
+
+/// Implement rating model class
+class Rating {
+  String userId;
+  String RatingType;
+  int rating;
+  String comment;
+
+  /// Rating constructor
+  Rating({required this.userId,
+    required this.RatingType,
+    required this.rating,
+    required this.comment
+  });
+}
+
