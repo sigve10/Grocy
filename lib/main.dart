0c41906e9ecce0c6a2929f0948ed274102c96a0a
emmanes <Emma.nesvik@hotmail.com>
2024-11-01 11:21:44 +0100

    Added login & account page
    Added supabase integration with login & account page, where magic link is currently used. Have commented out the code for the login to trigger, so other devs do not have to worry about this piece yet.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:grocy/screens/account_screen.dart';
+import 'package:grocy/screens/login_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+// Supabase redirects do not work with the default URL for flutter, so use this:
+// https://docs.flutter.dev/ui/navigation/url-strategies
+import 'package:flutter_web_plugins/url_strategy.dart';
 
-void main() {
+// If we want, we can set up RLS for security.
+// "These variables will be exposed on the app, and that's completely fine since we have Row Level Security enabled on our Database" - Supabase.
+const supabaseUrl = 'https://rmjqrlqmzuvmfutfvipl.supabase.co';
+const supabaseKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJtanFybHFtenV2bWZ1dGZ2aXBsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk5NDE0NDAsImV4cCI6MjA0NTUxNzQ0MH0.aXh9aJ7SywoceUaWe1N-5BlfsYHlbwpB7oPMQ4ofBeI';
+Future<void> main() async {
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
+  // For the redirects on web.
+  //TODO: set up android and IOS as well, before we push our project at the end.
+  usePathUrlStrategy();
   runApp(const MyApp());
 }
+
+final supabase = Supabase.instance.client;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -17,6 +34,11 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+
+      //Uncomment this and replace the home above for the login- & account pages.
+      /* home: supabase.auth.currentSession == null
+          ? const LoginPage()
+          : const AccountPage(), */
     );
   }
 }
@@ -31,13 +53,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+      ),
+    );
+  }
+}
+
+/// Snackbar extension, which shows snack bars in the app.
+/// TODO: define this in a separate file and import where needed.
+/// Used for the account & login page, so probably make a directory for it and put it there?
+extension ContextExtension on BuildContext {
+  void showSnackBar(String message, {bool isError = false}) {
+    ScaffoldMessenger.of(this).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: isError
+            ? Theme.of(this).colorScheme.error
+            : Theme.of(this).snackBarTheme.backgroundColor,
       ),
     );
   }
