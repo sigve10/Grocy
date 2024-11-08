df5021eb25b25346e5328bad84e02296a1bb23c2
stenOskar <stenhalse@gmail.com>
2024-11-08 01:40:34 +0100

    added imageUrl and review count to product.dart

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,19 +1,23 @@
 import 'package:grocy/models/rating.dart';
+import 'package:grocy/models/rating_set.dart';
 import 'package:grocy/models/review.dart';
 import 'package:grocy/models/tag.dart';
 
 class Product {
   final String key;
-
-  Product(
-    this.key,
-    this.name,
-    this.description
-  );
-
   final String name;
   final String description;
+  final String? imageUrl;
+  final int reviewCount;
   Set<Tag> tags = {};
   List<Review> reviews = [];
   List<Rating> ratings = [];
+
+  Product({
+    required this.key,
+    required this.name,
+    required this.description,
+    this.imageUrl,
+    this.reviewCount = 0,
+  });
 }
