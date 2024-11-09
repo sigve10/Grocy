947985eb85e3f6aade6dd896a60db815ea609eb4
emmanes <Emma.nesvik@hotmail.com>
2024-11-09 11:36:07 +0100

    Moved Snackbar Extension to its own file
    As it didn't really fit in the main file, and was only there temporarily, I have moved it to its own file and to use it you just need to import the file, and use context.showSnackbar.

---
+++ lib/extentions/snackbar_context.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+/// Snackbar extension, which shows snack bars in the app.
+/// Used for the account & login page, so far.
+extension ContextExtension on BuildContext {
+  void showSnackBar(String message, {bool isError = false}) {
+    ScaffoldMessenger.of(this).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: isError
+            ? Theme.of(this).colorScheme.error
+            : Theme.of(this).snackBarTheme.backgroundColor,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -51,19 +51,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-/// Snackbar extension, which shows snack bars in the app.
-/// TODO: define this in a separate file and import where needed.
-/// Used for the account & login page, so probably make a directory for it and put it there?
-extension ContextExtension on BuildContext {
-  void showSnackBar(String message, {bool isError = false}) {
-    ScaffoldMessenger.of(this).showSnackBar(
-      SnackBar(
-        content: Text(message),
-        backgroundColor: isError
-            ? Theme.of(this).colorScheme.error
-            : Theme.of(this).snackBarTheme.backgroundColor,
-      ),
-    );
-  }
-}
