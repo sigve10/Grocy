9a9484ae420724eff85d1dc7f02bdd6ec62641d7
sigvebjo <sigve@bjorkedal.org>
2024-11-20 15:13:58 +0100

    Made create_product scrollable

--- lib/screens/create_product_screen.dart
+++ lib/screens/create_product_screen.dart
@@ -38,110 +38,112 @@ class CreateProductScreenState extends ConsumerState<CreateProductScreen> {
   Widget build(BuildContext context) {
     final List<Tag> primaryTags = ref.watch(primaryTagProvider);
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 16.0),
-      child: Form(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Add a Product",
-              style: Theme.of(context).textTheme.titleLarge
-            ),
-            const SizedBox(height: 16.0),
-            DropdownButtonFormField(
-              hint: Text("Select a primary tag"),
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-                helper: selectedPrimaryTag == null ? Text(
-                  "Required",
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.error
-                  )
-                ) : null
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 16.0),
+        child: Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Add a Product",
+                style: Theme.of(context).textTheme.titleLarge
               ),
-              value: selectedPrimaryTag,
-              items: [
-                for (Tag tag in primaryTags)
-                  DropdownMenuItem<Tag>(
-                    value: tag,
-                    child: Text(tag.name),
-                  )
-              ],
-              onChanged: (tag) => setState( () =>
-                selectedPrimaryTag = tag
-              ),
-            ),
-            const SizedBox(height: 24.0),
-            const Divider(),
-            const SizedBox(height: 24.0),
-            TextField(
-              style: TextStyle(
-                backgroundColor: Theme.of(context).colorScheme.surfaceBright
-              ),
-              controller: nameController,
-              readOnly: true,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "Name",
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-                helperText: "Auto-filled",
-              ),
-            ),
-            const SizedBox(height: 24.0),
-            TextField(
-              controller: descController,
-              minLines: 5,
-              maxLines: 6,
-              expands: false,
-              readOnly: true,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-                labelText: "Description",
-                helperText: "Auto-filled",
-                hintText: "Product has no description"
-              ),
-            ),
-            const SizedBox(height: 24.0),
-            TextField(
-              controller: eanController,
-              readOnly: true,
-              keyboardType: const TextInputType.numberWithOptions(),
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "EAN (barcode)",
-                helperText: "Auto-filled",
-                floatingLabelBehavior: FloatingLabelBehavior.always
-              ),
-            ),
-            const Spacer(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Expanded(
-                  child: OutlinedButton(
-                    onPressed: onCancel,
-                    child: const Text(
-                      "Cancel"
+              const SizedBox(height: 16.0),
+              DropdownButtonFormField(
+                hint: Text("Select a primary tag"),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  helper: selectedPrimaryTag == null ? Text(
+                    "Required",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.error
                     )
-                  ),
+                  ) : null
                 ),
-                const SizedBox(width: 24.0),
-                Expanded(
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Theme.of(context).colorScheme.primary,
-                      foregroundColor: Theme.of(context).colorScheme.onPrimary
+                value: selectedPrimaryTag,
+                items: [
+                  for (Tag tag in primaryTags)
+                    DropdownMenuItem<Tag>(
+                      value: tag,
+                      child: Text(tag.name),
+                    )
+                ],
+                onChanged: (tag) => setState( () =>
+                  selectedPrimaryTag = tag
+                ),
+              ),
+              const SizedBox(height: 24.0),
+              const Divider(),
+              const SizedBox(height: 24.0),
+              TextField(
+                style: TextStyle(
+                  backgroundColor: Theme.of(context).colorScheme.surfaceBright
+                ),
+                controller: nameController,
+                readOnly: true,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Name",
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  helperText: "Auto-filled",
+                ),
+              ),
+              const SizedBox(height: 24.0),
+              TextField(
+                controller: descController,
+                minLines: 5,
+                maxLines: 6,
+                expands: false,
+                readOnly: true,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  labelText: "Description",
+                  helperText: "Auto-filled",
+                  hintText: "Product has no description"
+                ),
+              ),
+              const SizedBox(height: 24.0),
+              TextField(
+                controller: eanController,
+                readOnly: true,
+                keyboardType: const TextInputType.numberWithOptions(),
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "EAN (barcode)",
+                  helperText: "Auto-filled",
+                  floatingLabelBehavior: FloatingLabelBehavior.always
+                ),
+              ),
+              const SizedBox(height: 24.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Expanded(
+                    child: OutlinedButton(
+                      onPressed: onCancel,
+                      child: const Text(
+                        "Cancel"
+                      )
                     ),
-                    onPressed: onSubmit,
-                    child: const Text("Submit")
                   ),
-                )
-              ],
-            )
-          ],
+                  const SizedBox(width: 24.0),
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Theme.of(context).colorScheme.primary,
+                        foregroundColor: Theme.of(context).colorScheme.onPrimary
+                      ),
+                      onPressed: onSubmit,
+                      child: const Text("Submit")
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
