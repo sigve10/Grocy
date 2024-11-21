cad133ea92abc890bb0074474b4d5186e859aa1f
stenOskar <stenhalse@gmail.com>
2024-11-22 00:31:43 +0100

    Update rating.dart with getStarRating

--- lib/models/rating.dart
+++ lib/models/rating.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 class Rating {
   final String userId;
   final String productEan;
@@ -69,6 +72,27 @@ class Rating {
       ..labelAccuracyRating = (json['label_accuracy'] as num?)?.toDouble()
       ..priceRating = (json['price_accuracy'] as num?)?.toDouble()
       ..consistencyRating = (json['consistency'] as num?)?.toDouble();
+  }
+
+  static Widget getStarRating(double stars) {
+    const double starSize = 20;
+    const Color starColor = Colors.amber;
+
+    const Icon fullStar = Icon(Icons.star, size: starSize, color: starColor);
+    const Icon halfStar =
+    Icon(Icons.star_half, size: starSize, color: starColor);
+    const Icon noStar =
+    Icon(Icons.star_border, size: starSize, color: starColor);
+
+    int fullStars = stars.floor();
+    bool hasHalfStar = stars - (fullStars as double) >= 0.5;
+    int noStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
+
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      for (int i = 0; i < fullStars; i++) fullStar,
+      if (hasHalfStar) halfStar,
+      for (int i = 0; i < noStars; i++) noStar
+    ]);
   }
 
 }
