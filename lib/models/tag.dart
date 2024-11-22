9ac016d21acb2e091fcc02abd6e65b5cbc233795
stenOskar <stenhalse@gmail.com>
2024-11-22 15:40:14 +0100

    Update tag.dart with map

--- lib/models/tag.dart
+++ lib/models/tag.dart
@@ -7,11 +7,17 @@ class Tag {
     this.primaryTag
   });
 
-
   factory Tag.fromJson(Map<String, dynamic> json) {
     return Tag(
       name: json['name'] as String,
       primaryTag: json['primary_tag'] as String?,
     );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'primary_tag': primaryTag,
+    };
   }
 }
